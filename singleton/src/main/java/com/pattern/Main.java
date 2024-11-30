d5e1b019832b4a9ee00ab9e5408073a41d212a83
Viacheslav <korbutjava@gmail.com>
2024-11-30 10:15:18 +0200

    Reformated with new laptop

--- singleton/src/main/java/com/pattern/Main.java
+++ singleton/src/main/java/com/pattern/Main.java
@@ -2,15 +2,15 @@ package com.pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
-        "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
-        "RESULT:" + "\n");
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
 
-    Singleton singleton = Singleton.getInstance("First");
-    Singleton singleton1 = Singleton.getInstance("Second");
+        Singleton singleton = Singleton.getInstance("First");
+        Singleton singleton1 = Singleton.getInstance("Second");
 
-    System.out.println(singleton.value);
-    System.out.println(singleton1.value);
-  }
+        System.out.println(singleton.value);
+        System.out.println(singleton1.value);
+    }
 }
