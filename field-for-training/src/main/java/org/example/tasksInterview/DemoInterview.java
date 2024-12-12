6e8c1f52e6ed0836dcc09b6dd282a3cfee21196a
Viacheslav <korbutjava@gmail.com>
2024-12-12 18:03:46 +0200

    Implements first online code

--- field-for-training/src/main/java/org/example/tasksInterview/DemoInterview.java
+++ field-for-training/src/main/java/org/example/tasksInterview/DemoInterview.java
@@ -1,7 +1,11 @@
 package org.example.tasksInterview;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+//Відсортувати всі унікальні проєкти в методі uniqProjects()
+// де вхідним параметром буде List<Employee> зі списками проєктів
 public class DemoInterview {
     public static void main(String[] args) {
         List<Project> list = List.of(
@@ -10,6 +14,21 @@ public class DemoInterview {
                 new Project("JavaScript"));
         Employee e1 = new Employee(2000, list);
 
-        System.out.println("31:35 play with this position");
+        List<Project> list2 = List.of(
+                new Project("Java"),
+                new Project("JavaScript"),
+                new Project("Go"),
+                new Project("Piton")
+        );
+        Employee e2 = new Employee(2001, list2);
+
+        List<Employee> employees = List.of(e1, e2);
+        uniqProjects(employees).forEach(System.out::println);
+    }
+
+    public static Set<Project> uniqProjects(List<Employee> employees) {
+        return employees.stream()
+                .flatMap(employee -> employee.getProjects().stream())
+                .collect(Collectors.toSet());
     }
 }
--- field-for-training/src/main/java/org/example/tasksInterview/Project.java
+++ field-for-training/src/main/java/org/example/tasksInterview/Project.java
@@ -1,5 +1,7 @@
 package org.example.tasksInterview;
 
+import java.util.Objects;
+
 public class Project {
     String name;
 
@@ -13,5 +15,24 @@ public class Project {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Project project = (Project) o;
+        return Objects.equals(name, project.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
